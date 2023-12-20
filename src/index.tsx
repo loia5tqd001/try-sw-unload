93220832830e82e0c8a914370b3a6d2c391c15e5
Loi Nguyen Huynh <loia5tqd001@gmail.com>
2023-12-20 23:53:31 +0800

    add service worker playground

--- src/index.tsx
+++ src/index.tsx
@@ -17,9 +17,20 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+document.addEventListener('visibilitychange', (e) => {
+  if (document.visibilityState === 'hidden') {
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.active?.postMessage({ type: 'UNLOAD' });
+    });
+    navigator.sendBeacon(
+      `https://api.day.app/ViqUHaLVhHW5D244qhAzHK/UNLOAD_SENDBEACON_FROM_MAIN_THREAD`
+    );
+  }
+});
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -57,7 +57,8 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
+  ({ url }) =>
+    url.origin === self.location.origin && url.pathname.endsWith('.png'),
   // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: 'images',
@@ -74,6 +75,15 @@ registerRoute(
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
+  }
+  if (event.data && event.data.type === 'UNLOAD') {
+    fetch(`https://api.day.app/ViqUHaLVhHW5D244qhAzHK/UNLOAD_FETCH`);
+    fetch(`https://api.day.app/ViqUHaLVhHW5D244qhAzHK/UNLOAD_FETCH_KEEPALIVE`, {
+      keepalive: true,
+    });
+    navigator.sendBeacon(
+      `https://api.day.app/ViqUHaLVhHW5D244qhAzHK/UNLOAD_SENDBEACON_FROM_SW`
+    );
   }
 });
 
